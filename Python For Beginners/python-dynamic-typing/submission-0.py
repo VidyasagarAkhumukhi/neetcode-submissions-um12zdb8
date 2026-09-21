28a83cc83ed2a04b9fff15ff7cd15cd77f454b1c
Vidyasagar Akhumukhi <sagar112113@gmail.com>
2026-09-21 17:51:11 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 7
+print(type(variable))
+
+variable = 7.7
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "Ronaldo the Goat"
+print(type(variable))
+
+variable = [7, 10]
+print(type(variable))
