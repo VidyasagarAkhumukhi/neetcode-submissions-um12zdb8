368aae797e22c83738b9e231bb91b77ab9c4898b
Vidyasagar Akhumukhi <sagar112113@gmail.com>
2026-09-26 15:27:52 +0530

    Add: python-length-function - submission-0

---
+++ Python For Beginners/python-length-function/submission-0.py
@@ -0,0 +1,9 @@
+def get_longer_word(word1: str, word2: str) -> str:
+    print(len(word1), len(word2))
+
+
+
+# do not modify below this line
+print(get_longer_word("yellow", "orange"))
+print(get_longer_word("red", "blue"))
+print(get_longer_word("green", "blue"))
